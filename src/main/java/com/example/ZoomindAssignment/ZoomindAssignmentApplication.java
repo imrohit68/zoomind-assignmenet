dab0f32053f9df630086cfb6717d27546806b2b6
Rohit Singh <sirohit328@gmail.com>
2025-03-22 21:09:28 +0530

    minor-changes

--- src/main/java/com/example/ZoomindAssignment/ZoomindAssignmentApplication.java
+++ src/main/java/com/example/ZoomindAssignment/ZoomindAssignmentApplication.java
@@ -1,15 +1,11 @@
 package com.example.ZoomindAssignment;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
@@ -17,9 +13,13 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @EnableCaching
 @OpenAPIDefinition(
 		info = @Info(
-				title = "Test Case Management API",
+				title = "🔍 Test Case Management API",
 				version = "1.0",
-				description = "This API allows users to manage test cases, including creating, retrieving, updating, and deleting test cases."
+				description = "A RESTful API designed to efficiently manage software test cases. Supports CRUD operations, pagination, filtering, and caching for optimal performance.",
+				contact = @Contact(
+						name = "Rohit Singh",
+						email = "sirohit328@gmail.com"
+				)
 		)
 )
 public class ZoomindAssignmentApplication {
